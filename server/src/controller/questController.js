57f05aeab1a249cd05dca2f0b0b4f96630d700fb
차상원 <cktkddnjs1@icloud.com>
2022-06-14 20:34:26 +0900

    수정: server project 제거

--- server/src/controller/questController.js
+++
@@ -1,6 +0,0 @@
-export const create = (req, res, next) => {};
-export const read = (req, res, next) => {
-  res.json("아이고");
-};
-export const readDetail = (req, res, next) => {};
-export const accept = (req, res, next) => {};
--- src/apis/url.js
+++ src/apis/url.js
@@ -1,5 +1,6 @@
 // import { BASE_URL } from "@env";
 
 let ROOT_URL = "http://localhost:8080";
+ROOT_URL = "https://www.main-bvxea6i-kbizbope644fs.au.platformsh.site";
 // ROOT_URL = BASE_URL || "http://localhost:8080";
 export { ROOT_URL };
